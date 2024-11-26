b0f4148132cd5376aeecba4b601b5be6d2a6660f
HENRY MUTEGA <hmutega@gmail.com>
2024-11-26 21:05:59 +0300

    order controller to create new orders.

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	"context"
+	"elogistics-backend/database"
 	"elogistics-backend/models"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func CreateOrder(c *gin.Context) {
@@ -13,4 +17,17 @@ func CreateOrder(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	order.ID = primitive.NewObjectID()
+	order.Status = "Pending"
+	order.CreatedAt = primitive.NewDateTimeFromTime(time.Now())
+
+	collection := database.Client.Database("elogistics").Collection("orders")
+	_, err := collection.InsertOne(context.Background(), order)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create order"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Order created successfully"})
 }
