d4b2af86af074d65346d9d2614ee64adecc7f600
HENRY MUTEGA <hmutega@gmail.com>
2024-11-26 20:27:19 +0300

    Project initialization.

---
+++ database/mongodb.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func ConnectMongoDB() {
+	uri := os.Getenv("MONGODB_URI")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var err error
+	Client, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB: %V", err)
+	}
+
+	log.Println("Connected to MongoDB!")
+
+}
